b605d96edaf86bb580af386da11fa40c8dc3b595
EdRafCas <Eduardoo.rafael@gmail.com>
2021-05-18 20:34:13 -0300

    accessin the firebase database trough the snapshot method

--- src/Componentes/ListaContactos.js
+++ src/Componentes/ListaContactos.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import styled from 'styled-components';
 import db from './../firebase/firebaseConfig';
 import Contacto from './Contacto'
@@ -10,6 +10,14 @@ const ListaContactos = () => {
             {id: 2, nombre: "Eduardo", correo:"correo"}
       ]);
 
+      useEffect(()=> {
+            db.collection('usuarios').limit(2).onSnapshot((snapshot)=>{
+                  cambiarContactos(snapshot.docs.map((documento)=>{
+                        return {...documento.data(), id: documento.id}
+                  }));
+            })
+
+      }, []) // los corchetes hacen que solo se ejecute cuando cargue por primera vez
 
       return ( 
             contactos.length > 0 &&
